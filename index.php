2330e3660d2d91bf8aa313e81c4a11d3eefd0edc
DYOU23 <dyovani.integrae@gmail.com>
2025-02-21 18:00:42 +0100

    act

---
+++ index.php
@@ -0,0 +1,37 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+ </head>
+ <body>
+  
+ <div class="container px-4 text-center">
+
+  <div class="row gx-2">
+  <legend class="mt-4">Universidad</legend>
+    <div class="col">
+      <div class="gap-3 mt-4">
+        <a href="alumnos.php" class="btn btn-outline-primary container">Alumnos</a>
+      </div>
+    </div>
+    <div class="col">
+      <div class="d-grid gap-3 mt-4">
+        <a href="profesores.php" class="btn btn-outline-success container">Profesores</a>
+      </div>
+    </div>
+    <div class="d-grid gap-3 mt-2">
+      <a href="busquedas.php" class="btn btn-outline-secondary container">Consultar Datos</a>
+    </div>
+  </div>
+</div>
+
+
+      
+ </body>
+ </html>
