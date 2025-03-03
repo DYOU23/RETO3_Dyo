e94b778dda27ad5233e8d43f54ddd3ba399ae1ea
DYOU23 <dyovani.integrae@gmail.com>
2025-03-03 13:53:53 +0100

    act

--- conexion.php
+++ conexion.php
@@ -1,6 +1,20 @@
 
 <?php
 
+/*
+$username = "root";
+$password = "";
+$database = "instituto";
+$host = "localhost";
+
+EXAMEN: Para que no se pueda cambiar los datos de la conexion de mi BD, uso define o const:
+
+define("username", "root");
+define("password", "");
+define("database", "instituto");
+define("host", "localhost");
+*/
+
 const username = "root";
 const password = "";
 const database = "universidad";
